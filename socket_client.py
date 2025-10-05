9d7c08679ccef4e2755c441d0fbaaf72b93581d6
Aidan Rohm <aidanrohm@gmail.com>
2025-10-05 13:22:10 -0400

    Update socket_client.py
    Per assignment description

--- socket_client.py
+++ socket_client.py
@@ -7,16 +7,18 @@ ip_port = ('127.0.0.1', 9999)
 s = socket.socket()
 s.connect(ip_port)
 
+client_id = s.recv(1024).decode()
+print(client_id)
+
 while True:
-    inp = input('input msg： ').strip()
+    inp = input("Enter command: ").strip()
     if not inp:
         continue
     s.sendall(inp.encode())
 
-    if inp == "exit":
-        print("communication end！")
-        break
-
-    server_reply = s.recv(1024).decode()
+    server_reply = s.recv(4096).decode()
     print(server_reply)
+
+    if inp == "exit":
+        break
 s.close()
