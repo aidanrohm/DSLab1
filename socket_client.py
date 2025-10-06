b1be91270679240c0c488b9be5e28e1dd6d93d3f
Aidan Rohm <aidanrohm@gmail.com>
2025-10-05 21:07:01 -0400

    Update socket_client.py
    Adding descriptive comments regarding the development of the code

--- socket_client.py
+++ socket_client.py
@@ -13,6 +13,9 @@ client_id = s.recv(1024).decode()
 print(client_id)
 
 # Background thread to listen for server messages
+# This function was implemented after some trial and error regarding the receiving of messages
+# Without using a thread for message reception, the user needed to enter a command or key
+# in order for the message to be displayed
 def listen_for_messages(sock):
     while True:
         try:
@@ -24,11 +27,13 @@ def listen_for_messages(sock):
         except:
             break
 
+# Using a thread to listen for messages, this allows the client to immediately display a message
 threading.Thread(target=listen_for_messages, args=(s,), daemon=True).start()
 
 # Main loop for sending commands
 while True:
     try:
+        # Standard user input to communicate over the connection
         inp = input("Enter command: ").strip()
         if not inp:
             continue
